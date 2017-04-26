f90ff0cfcf390c0097de0cb0fcfcd495b552916a
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-26 11:34:09 -0400

    added a playlist functio to my jukebox

--- scripts.js
+++ scripts.js
@@ -4,17 +4,25 @@ var skipButton = document.getElementById("skipButton");
 var loadButton = document.getElementById("loadButton");
 var audio = document.getElementById("player");
 
-playButton.addEventListener("click", play);
-pauseButton.addEventListener("click", pause);
-skipButton.addEventListener("click", skip);
-// loadButton.addEventListener("click", load);
+// -------Song Object Instances-------
+
+var ego = new Song ("Ego Likeness", "Dragonfly", "Drown Like You", "assets/audio/drown_like_you.mp3", "assets/images/egolikeness.jpg");
+var horrors = new Song ("The Horrors", "Skying", "Still Life", "assets/audio/still_life.mp3", "assets/images/horrors.jpg");
+var dreamside = new Song ("The Dreamside", "Spin Moon Magic", "Into a Frenzy", "assets/audio/into_a_frenzy.mp3", "assets/images/dreamside.jpg");
+
+var songs = [ego, horrors, dreamside];
+var currentSong = 0;
+
+window.onload = function() {
+	audio.src = songs[currentSong].fileName;
+};
+
+// -------Object Constructors-------
 
 function Jukebox() {
 	this.play = play;
 	this.pause = pause;
 	this.skip = skip;
-	this.songs = [];
-	this.currentSong = 0;
 }
 
 function Song(band, album, song, fileName, artFile) {
@@ -25,23 +33,33 @@ function Song(band, album, song, fileName, artFile) {
 	this.artFile = artFile;
 }
 
-function songList() {
-	for(var i=0; i < playlist.length; i++) {
-		console.log(playlist[i].song)
-	}
-}
-
 // ----------Jukebox Functionality-------------
 
 var jukebox = new Jukebox();
 
+playButton.addEventListener("click", play);
+pauseButton.addEventListener("click", pause);
+skipButton.addEventListener("click", skip);
+// loadButton.addEventListener("click", load);
+
+var displayText = document.getElementsByClassName("display-text");
+var albumArt = document.getElementsByClassName("albumArt")[0];
+
+function display(songs) {
+	albumArt.src = songs[currentSong].artFile;
+	displayText[0].innerHTML = songs[currentSong].song;
+	displayText[1].innerHTML = songs[currentSong].band;
+	displayText[2].innerHTML = songs[currentSong].album;
+}
+
 function play() {
 	audio.src = songs[currentSong].fileName;
-	jukebox.play()
+	audio.play()
+	display(songs)
 }
 
 function pause() {
-	jukebox.pause()
+	audio.pause()
 }
 
 function skip() {
@@ -55,14 +73,31 @@ function skip() {
 	}
 }
 
-// -------Song Object Instances-------
+var song = document.getElementById("song").value; 
+var band = document.getElementById("band").value;
+var album = document.getElementById("album").value;
+var songFile = document.getElementById("file-name").value;
+var artFile = document.getElementById("art-file").value;
+var loadButton = document.getElementById("load-song");
 
-var ego = new Song ("Ego Likeness", "Dragonfly", "Drown Like You", "assets/audio/drown_like_you.mp3", "assets/images/egolikeness.jpg");
-var horrors = new Song ("The Horrors", "Skying", "Still Life", "assets/audio/still_life.mp3", "assets/images/horrors.jpg");
-var dreamside = new Song ("The Dreamside", "Spin Moon Magic", "Into a Frenzy", "assets/audio/into_a_frenzy.mp3", "assets/images/dreamside.jpg");
+// loadButton.addEventListener("click", load);
 
+// function load() {
+// 	var newSong = new Song(band, album, song, songFile, artFile);
 
+// 	songs.push(newSong);
+// 	console.log(songs);
+// }
 
+var playlistItem = document.getElementsByClassName("playlist-item")[0];
+
+function songList(playlist) {
+	for(var i=0; i < playlist.length; i++) {
+		playlistItem.innerHTML += "<p>" + playlist[i].band + " : " + playlist[i].song + "</p>";
+	}
+}
+
+songList(songs);
 
 
 
